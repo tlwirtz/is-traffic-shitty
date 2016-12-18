f79532b8e73a1edf22f3eb7079b8ed734d5c6313
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-12-18 13:47:17 -0800

    updat title and serve over https

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ import '../css/App.css';
 import request from 'superagent'
 import jsonp from 'superagent-jsonp'
 
-const WSDOT_URL = 'http://wsdot.com/Traffic/api/TravelTimes/TravelTimesREST.svc'
+const WSDOT_URL = 'https://wsdot.com/Traffic/api/TravelTimes/TravelTimesREST.svc'
 const ACCESS_CODE = process.env.WSDOT_ACCESS_CODE || '10a60c16-25f1-49c3-bcbe-b83f01e77f7e'
 
 class App extends Component {
