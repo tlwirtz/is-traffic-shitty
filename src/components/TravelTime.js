a0b30bba63ce25b9d343f4e26b5b8ee7cee10af5
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-01-02 12:37:04 -0800

    re-do traffic list to be more meaningful

--- src/components/TravelTime.js
+++ src/components/TravelTime.js
@@ -1,26 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classNames from 'classnames'
 import '../css/TravelTime.css'
 
-const TravelTime = (props) => {
-  const isGood = (avg, current) => { return avg >= current }
-  const isBad = (avg, current) => { return ((avg + 10) <= current) }
-  const isWarning = (avg, current) => { return (current > avg) && (current < avg + 10) }
-  const { item } = props
-  const travelTime = classNames({
-    TravelTime: true,
-    red: isBad(item.AverageTime, item.CurrentTime),
-    green: isGood(item.AverageTime, item.CurrentTime),
-    yellow: isWarning(item.AverageTime, item.CurrentTime)
-  })
+class TravelTime extends Component {
+  constructor() {
+    super()
 
-  return (
-    <li className={travelTime}>
-      <h2 className="travel-title">{item.Description} - {item.TravelTimeID}</h2>
-      <p>Average Time: {item.AverageTime}</p>
-      <p>Current Time: {item.CurrentTime}</p>
-    </li>
-  )
+    this.formatMsg = this.formatMsg.bind(this)
+    this.status = this.status.bind(this)
+    this.diff = this.diff.bind(this)
+  }
+
+  diff(a,b) {
+    return a - b
+  }
+
+  status(avg, current) {
+    const d = this.diff(current, avg)
+    if (d > 10)  return 'bad'
+    if (d > 0 )   return 'warning'
+    if (d <= 0 )  return 'good'
+  }
+
+  formatMsg(item) {
+    const {AverageTime, CurrentTime} = item
+    const d = this.diff(CurrentTime, AverageTime)
+    let msg
+    const travelTime = classNames({
+        'red-text': this.status(AverageTime, CurrentTime) === 'bad',
+        'green-text': this.status(AverageTime, CurrentTime) === 'good',
+        'yellow-text': this.status(AverageTime, CurrentTime) === 'warning'
+      })
+
+    if (d === 0) msg = `average.`
+    if (d > 0) msg = `${Math.abs(d)} ${ d === 1 ? 'minute' : 'minutes'} above average.`
+    if (d < 0) msg = `${Math.abs(d)} ${ d === -1 ? 'minute' : 'minutes'} below average.`
+    return <span className={travelTime}>{msg}</span>
+  }
+
+  render() {
+    const { item } = this.props
+    const travelTime = classNames({
+      TravelTime: true
+    })
+    const msg = this.formatMsg(item, item.AverageTime, item.CurrentTime)
+    return (
+      <li className={travelTime}>
+        <h2>{item.Description} is {msg}</h2>
+      </li>
+    )
+  }
 }
 
 export default TravelTime
