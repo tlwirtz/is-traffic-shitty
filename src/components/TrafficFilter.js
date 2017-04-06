76a745fa0151207ed056eda69ab364a848230773
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-04-05 18:51:13 -0700

    formatting fixes

--- src/components/TrafficFilter.js
+++ src/components/TrafficFilter.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import '../css/TravelTimeFilter.css'
+import React, { Component } from 'react';
+import '../css/TravelTimeFilter.css';
 
 class TrafficFilter extends Component {
   render() {
@@ -8,8 +8,8 @@ class TrafficFilter extends Component {
         <span className="search-icon"><i className="fa fa-search"></i></span>
         <input className="travel-filter" type='text' placeholder="Search" onChange={(e) => this.props.filterTraffic(e)} />
       </div>
-    )
+    );
   }
 }
 
-export default TrafficFilter
+export default TrafficFilter;
